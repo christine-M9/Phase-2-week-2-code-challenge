296cdf97ff099ea80d13a845e7bfa5b65a0d9bc3
Christine <christinekhekama99@gmail.com>
2023-07-31 00:29:55 +0300

    refetching

--- bot/src/BotCollection.js
+++ bot/src/BotCollection.js
@@ -14,15 +14,18 @@ const BotCollection = ({ onlistBot }) => {
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))', gridGap: '20px', marginBottom: '30px' }}>
       {bots.map((bot) => (
         <div key={bot.id} onClick={() => onlistBot(bot)} style={{ border: '1px solid #ddd', padding: '10px', cursor: 'pointer' }}>
+          
+          <p>ID: {bot.id}</p>
           <h2>{bot.name}</h2>
-          <p>Description: {bot.bot_class}</p>
           <p>Health: {bot.health}</p>
           <p>Damage: {bot.damage}</p>
           <p>Armor: {bot.armor}</p>
+          <p>Bot_class: {bot.bot_class}</p>
           <p>Catchphrase: {bot.catchphrase}</p>
+          <img src={bot.avatar_url} alt={bot.name} style={{ maxWidth: '40%', height: 'auto' }} />
           <p>Created At: {new Date(bot.created_at).toLocaleString()}</p>
           <p>Updated At: {new Date(bot.updated_at).toLocaleString()}</p>
-          <img src={bot.avatar_url} alt={bot.name} style={{ maxWidth: '40%', height: 'auto' }} />
+         
         </div>
       ))}
     </div>
